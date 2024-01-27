a9d932c1beba75ebc615ec067c946ef6aa60cc66
dvillegas.it <dvillegas@hotelbeds.com>
2024-01-27 21:11:31 +0100

    test ndef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from "@angular/forms";
+import {NfcService} from "./services/nfc.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'spoolmeter';
+
+  public logs = "";
+
+  constructor(private nfcService: NfcService) {
+  }
+
+  public scan() {
+    this.nfcService.readTag(10000, this.logs).subscribe();
+  }
+
 }
