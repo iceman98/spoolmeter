696c075cc6d0f3c13bd43e39424ce3ff234b7594
dvillegas.it <dvillegas@hotelbeds.com>
2024-02-10 22:47:28 +0100

    help modal

---
+++ src/app/components/help-dialog/help-dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from "@angular/material/dialog";
+import {MatButton} from "@angular/material/button";
+
+@Component({
+  selector: 'app-help-dialog',
+  standalone: true,
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatButton,
+    MatDialogClose
+  ],
+  templateUrl: './help-dialog.component.html',
+  styleUrl: './help-dialog.component.sass'
+})
+export class HelpDialogComponent {
+
+}
