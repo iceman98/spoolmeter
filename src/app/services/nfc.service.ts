19baeef7a5f110ff1f6442c2a04fc7022cc6616e
dvillegas.it <dvillegas@hotelbeds.com>
2024-02-11 01:14:02 +0100

    simplification of NfcService

--- src/app/services/nfc.service.ts
+++ src/app/services/nfc.service.ts
@@ -1,10 +1,11 @@
 import {Injectable} from '@angular/core';
-import {AsyncSubject, Observable} from "rxjs";
+import {AsyncSubject, BehaviorSubject, Observable, of, Subject, throwError} from "rxjs";
 import {Spool} from "../model/spool";
 import {NfcEmulatorService} from "./nfc-emulator.service";
 import {EmulatedNdefReader} from "../classes/emulated-ndef-reader";
 import {environment} from "../../environments/environment";
 import {MessageConverter} from "../classes/message-converter";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 
 @Injectable({
@@ -13,21 +14,14 @@ import {MessageConverter} from "../classes/message-converter";
 export class NfcService {
 
   private ndef: NDEFReader;
-  private reading = false;
-  private read = false;
-
-  private writing = false;
-  private written = false;
-
-  private writingTo?: string;
-  private writingBody?: NDEFMessageInit;
-
   private converter = new MessageConverter();
 
-  private readSubject?: AsyncSubject<Spool>;
-  private writeSubject?: AsyncSubject<void>;
+  private statusSubject = new BehaviorSubject("Idle");
 
-  constructor(private nfcEmulator: NfcEmulatorService) {
+  private readSubject = new Subject<Spool>();
+  private writing?: { id: string, message: NDEFMessageInit, subject: AsyncSubject<void> };
+
+  constructor(private nfcEmulator: NfcEmulatorService, private snackBar: MatSnackBar) {
     // @ts-ignore
     this.ndef = environment.isDev ? new EmulatedNdefReader(this.nfcEmulator) : new NDEFReader();
     this.ndef.onreading = tag => this.handleReadWrite(tag);
@@ -35,80 +29,67 @@ export class NfcService {
 
     this.ndef.scan()
       .then(() => {
-        console.log("Starting scan...");
+        this.snackBar.open("Starting scan...", "Dismiss", {duration: 1000});
+        this.statusSubject.next("Scanning");
       })
       .catch((e: Event) => {
+        this.snackBar.open("Scan could not be started: " + e.type, "Dismiss", {duration: 1000});
         console.log("Scan could not be started: " + e.type);
+        this.statusSubject.next("Error");
       });
   }
 
-  public readSpool(timeout: number): Observable<Spool> {
-    this.readSubject = new AsyncSubject<Spool>();
-
-    if (!this.reading) {
-      this.reading = true;
-      this.read = false;
-      setTimeout(() => {
-        if (!this.read) {
-          console.warn("Timeout!");
-          this.readSubject?.error("Read timed out");
-        }
-        this.reading = false;
-        this.read = false;
-      }, timeout);
-    } else {
-      console.warn("Read in progress!")
-      this.readSubject.error("Already reading");
-    }
-
+  public spool$(): Observable<Spool> {
     return this.readSubject;
   }
 
+  public status$(): Observable<string> {
+    return this.statusSubject;
+  }
+
   public updateSpool(spool: Spool): Observable<void> {
-    this.writeSubject = new AsyncSubject<void>();
+    if (!this.writing) {
+      this.statusSubject.next("Writing");
 
-    let message = this.converter.spoolToMessage(spool);
+      this.writing = {
+        id: spool.id,
+        message: {records: this.converter.spoolToMessage(spool).records.map(r => r as NDEFRecordInit)},
+        subject: new AsyncSubject<void>(),
+      };
 
-    const source: NDEFMessageInit = {
-      records: message.records.map(r => {
-        return r as NDEFRecordInit
-      })
-    };
-
-    this.writing = true;
-    this.writingTo = spool.id;
-    this.writingBody = source;
-
-    return this.writeSubject;
+      return this.writing.subject;
+    } else {
+      return throwError(new Error("Already writing"));
+    }
   }
 
   handleReadWrite(tag: NDEFReadingEvent) {
-    if (this.reading && !this.read) {
-      this.read = true;
-      this.readSubject?.next(this.converter.messageToSpool(tag.serialNumber, tag.message));
-      this.readSubject?.complete();
+    if (!this.writing) {
+      this.readSubject.next(this.converter.messageToSpool(tag.serialNumber, tag.message));
     }
 
-    if (this.writing && !this.written && this.writingTo && this.writingBody) {
-      if (tag.serialNumber === this.writingTo) {
-        this.ndef.write(this.writingBody, {overwrite: true})
+    if (this.writing) {
+      if (tag.serialNumber === this.writing.id) {
+        this.ndef.write(this.writing.message, {overwrite: true})
           .then(() => {
-            this.written = true;
-            this.writeSubject?.next();
-            this.writeSubject?.complete();
+            this.statusSubject.next("Reading");
+            this.writing?.subject.complete();
+            this.writing = undefined;
           })
           .catch((e) => {
             console.log(e);
+            this.writing?.subject.error(new Error("Could not write tag, try again?"));
           })
           .finally();
       } else {
-        console.log("This is not the tag we are waiting for");
+        this.snackBar.open("Not the correct tag, try again", "Dismiss", {duration: 1000});
       }
     }
   }
 
   handleError(error: Event) {
-    console.error("Could not read tag: " + error.type);
+    console.log(error);
+    this.snackBar.open("Error reading tag, try again?", "Dismiss", {duration: 1000});
   }
 
 }
